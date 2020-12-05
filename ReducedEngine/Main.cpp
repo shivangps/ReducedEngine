433771d7cc3c7ad11e374406386e4630433f7662
shivangps <shivangps@gmail.com>
2020-12-05 21:24:01 +0530

    Add project files.

---
+++ ReducedEngine/Main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+// Main function.
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	OutputDebugStringA("\nSup World!!\n");
+
+	return 0;
+}
